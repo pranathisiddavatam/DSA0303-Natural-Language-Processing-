7b38746b1c58b0a4f5141f57d6934fea553ab845
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-10-12 08:38:55 +0530

    Delete partofspeech.py

--- partofspeech.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk import word_tokenize
-from nltk import pos_tag
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-text = "NLTK is a leading platform for building Python programs to work with human language data."
-words = word_tokenize(text)
-pos_tags = pos_tag(words)
-print(pos_tags)
