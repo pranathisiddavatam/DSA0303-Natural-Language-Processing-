8ca412b2def1fdc98935de7467cd5ffb0cbab2ad
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-10-12 08:39:33 +0530

    Delete stemmer.py

--- stemmer.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.stem import PorterStemmer
-nltk.download('punkt')
-stemmer = PorterStemmer()
-words = ["jumping", "jumps", "jumped", "running", "runner", "easily"]
-stemmed_words = [stemmer.stem(word) for word in words]
-for word, stemmed_word in zip(words, stemmed_words):
-    print(f"{word} => {stemmed_word}")
-
